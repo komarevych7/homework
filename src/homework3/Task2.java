c5bca77bea49e07ee375f10ade60001c4bf8bd2e
komarevych7 <76686669+komarevych7@users.noreply.github.com>
2021-08-16 21:43:17 +0300

    tast 1,2

--- src/homework3/Task2.java
+++ src/homework3/Task2.java
@@ -2,16 +2,17 @@ package homework3;
 
 import java.util.Scanner;
 
-public class Task1 {
+public class Task2 {
     public static void main(String[] args) {
         int hours;
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter a time in format \"HH\"" );
         hours = scanner.nextInt();
-
-        if (hours <= 18 && hours >=0) {
+        if (hours >= 0 && hours <=10) {
+            System.out.println("Good morning");
+        } else if (hours > 10 && hours <=18) {
             System.out.println("Good day");
-        } else if (hours > 18.00 && hours <=24) {
+        } else if (hours > 18 && hours <=24) {
             System.out.println("Good evening");
         } else {
             System.out.println("Error. Use correct time format (e.g. 18.30)");

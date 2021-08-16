a787d8ea0c278f165f52d513a56392f698bb5be8
komarevych7 <76686669+komarevych7@users.noreply.github.com>
2021-08-16 20:58:09 +0300

    test

--- src/homework3/Task1.java
+++ src/homework3/Task1.java
@@ -7,5 +7,13 @@ public class Task1 {
         double time;
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter a time in format \"HH.MM\"" );
+        time = scanner.nextDouble();
+        if (time <= 18.00 && time >=00.00) {
+            System.out.println("Good day");
+        } else if (time >= 18.00 && time <=24.00) {
+            System.out.println("Good evening");
+        } else {
+            System.out.println("Error. Use correct time format (e.g. 18.30)");
+        }
     }
 }

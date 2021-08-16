5f07318bcf126121c36d79703f88bab3123534ae
komarevych7 <76686669+komarevych7@users.noreply.github.com>
2021-08-16 20:32:55 +0300

    test

---
+++ src/homework3/Task1.java
@@ -0,0 +1,11 @@
+package homework3;
+
+import java.util.Scanner;
+
+public class Task1 {
+    public static void main(String[] args) {
+        double time;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a time in format \"HH.MM\"" );
+    }
+}

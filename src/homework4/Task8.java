8ee7bb4e393309fde8a0c1c949956f44cfcc4f8e
komarevych7 <76686669+komarevych7@users.noreply.github.com>
2021-09-14 12:45:52 +0300

    hw4 task8

--- src/homework4/Task8.java
+++ src/homework4/Task8.java
@@ -6,20 +6,26 @@ public class Task8 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int number = (int) (Math.random() * 10) + 1;
-        // System.out.println(number);
+        //System.out.println(number);
+        System.out.println("Try to guess a number from 0 to 10");
         int userNumber = scan.nextInt();
+        boolean b = false;
 
         do {
 
-            if (number == userNumber) {
-                System.out.println("True");
-                break;
+            if ( number == userNumber ) {
+                System.out.println("Correctly");
+                b = true;
+            } else if ( userNumber > 10 || userNumber < 0 ) {
+                System.out.println("Number should be from 0 to 10. Try again");
+            } else if ( number < userNumber ) {
+                System.out.println("Too high, try again.");
             } else {
-                System.out.println("Loh");
-                userNumber = scan.nextInt();
+                System.out.println("Too low, try again.");
             }
+            userNumber = scan.nextInt();
 
-        } while (number == userNumber);
+        } while ( b != true);
 
     }
 }

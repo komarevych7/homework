f783310b6bcf550ad1b9189788f5b9eaca0f8941
komarevych7 <76686669+komarevych7@users.noreply.github.com>
2021-09-14 10:40:53 +0300

    hw4 task7 fix

--- src/homework4/Task7.java
+++ src/homework4/Task7.java
@@ -5,24 +5,32 @@ import java.util.Scanner;
 public class Task7 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        System.out.println("Enter array size");
         int size = scan.nextInt();
         int positive = 0;
         int negative = 0;
         int zero = 0;
-        int n = 0;
-        int[] arr = {1, 3, 0, -1, 5, 0, 3};
+        int attempt = 0;
+        int arr[] = new int[size];
 
-        do { n++;
+        System.out.println("Enter array");
+        for (int i = 0; i < size; i++) {
+            arr[i] = scan.nextInt();
+        }
 
-            if (arr[n] > 0) {
+        do {
+
+            if (arr[attempt] > 0) {
                 positive ++;
-            } else if (arr[n] < 0) {
+            } else if (arr[attempt] < 0) {
                 negative ++;
             } else {
                 zero ++;
             }
+            attempt++;
 
-        } while ( n < size );
-        System.out.println(" i " + positive + " j "+ negative + " z "+ zero);
+        } while ( attempt < size );
+
+        System.out.println(" Positive " + positive + " Negative "+ negative + " Zero "+ zero);
     }
 }
